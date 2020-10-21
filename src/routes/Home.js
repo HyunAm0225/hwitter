2efb20ba4291e5f1c37be681aeb0701e2a7c75cb
HyunAm <tlfgjawlq@naver.com>
2020-10-21 13:04:12 +0900

    3.0 파이어베이스 데이터베이스 세팅

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [hweet, setHweet] = useState("");
+  const onSubmit = event => {
+    event.preventDefault();
+  };
+  const onChange = event => {
+    const {
+      target: { value },
+    } = event;
+    setHweet(value);
+  };
+  //   const
+  return (
+    <div>
+      <form>
+        <input type="text" onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
+        <input type="submit" value="Hweet" />
+      </form>
+    </div>
+  );
+};
+
 export default Home;
