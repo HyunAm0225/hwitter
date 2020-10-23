196155e076cb988cabbf91604b75db64b69ce621
HyunAm <tlfgjawlq@naver.com>
2020-10-23 16:40:54 +0900

    클린코딩 하기, 로그아웃 기능 개선

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { dbService, storageService } from "fbase";
+import { dbService } from "fbase";
 import Hweet from "components/hweet";
-import { v4 as uuidv4 } from "uuid";
+import HweetFactory from "components/HweetFactory";
 
 const Home = ({ userObj }) => {
-  const [hweet, setHweet] = useState("");
   const [hweets, setHweets] = useState([]);
-  const [attachment, setAttachment] = useState("");
 
   useEffect(() => {
     dbService.collection("hweets").onSnapshot(snapshot => {
@@ -17,60 +15,10 @@ const Home = ({ userObj }) => {
       setHweets(hweetArray);
     });
   }, []);
-  const onSubmit = async event => {
-    event.preventDefault();
-    let attachmentUrl = "";
-    if (attachment !== "") {
-      const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
-      const response = await attachmentRef.putString(attachment, "data_url");
-      attachmentUrl = await response.ref.getDownloadURL();
-    }
-    const hweetObj = {
-      text: hweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-      attachmentUrl,
-    };
-    // console.log(hweetObj.attachmentUrl);
 
-    await dbService.collection("hweets").add(hweetObj);
-    setHweet("");
-    setAttachment("");
-  };
-  const onChange = event => {
-    const {
-      target: { value },
-    } = event;
-    setHweet(value);
-  };
-  const onFileChange = event => {
-    const {
-      target: { files },
-    } = event;
-    const theFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = finishedEvent => {
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    reader.readAsDataURL(theFile);
-  };
-  const onClearAttachmentClick = () => setAttachment(null);
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input type="text" value={hweet} onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
-        <input type="file" accept="image/*" onChange={onFileChange} />
-        <input type="submit" value="Hweet" />
-        {attachment && (
-          <div>
-            <img src={attachment} width="50px" height="50px" />
-            <button onClick={onClearAttachmentClick}>Clear</button>
-          </div>
-        )}
-      </form>
+      <HweetFactory userObj={userObj} />
       <div>
         {hweets.map(hweet => (
           <Hweet key={hweet.id} hweetObj={hweet} isOwner={hweet.creatorId === userObj.uid} />
